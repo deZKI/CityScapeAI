6c29ac2aae569c15e76c07bf5eba5a1955d83c97
Марьян Рощупкин <roshchupkin.marian@gmail.com>
2024-11-10 02:20:57 +1000

    feat(application): добавляет логику для отображения разного текста в панели доступности

--- frontend/src/ui/Panel/Availability/Availability.tsx
+++ frontend/src/ui/Panel/Availability/Availability.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import './availability.css';
 import {TPanelStyles} from "../../../types/types/TPanelStyles.type";
 import Panel from "../Panel";
+import {useSelector} from "react-redux";
+import {TInitialState} from "../../../store/reducer";
+import {EModeSwitcher} from "../../../types/enums/EModeSwitcher.enum";
 
 export default function Availability() {
-  const styles: TPanelStyles = { right: '16px', bottom: '16px', padding: '16px', width: '258px' }
+  const modeSwitcher = useSelector<TInitialState, EModeSwitcher>(state => state.modeSwitcher.modeSwitcher)
+  const styles: TPanelStyles = {
+    right: '16px',
+    bottom: '16px',
+    padding: '16px',
+    width: modeSwitcher === EModeSwitcher.polygons ? '258px' : '420px'
+  }
 
   return (
-    <Panel title='Доступность' subtitle='услов.ед' showTitle={true} showMenu={false} styles={styles}>
+    <Panel
+      title={`${modeSwitcher === EModeSwitcher.polygons ? 'Доступность' : 'Плотность значимых объектов'}`}
+      subtitle='услов.ед' showTitle={true}
+      showMenu={false}
+      styles={styles}
+    >
       <div className='panel__content__units'>
         <span className='panel__content__unit'>&gt; 225</span>
         <span className='panel__content__unit'>276</span>
